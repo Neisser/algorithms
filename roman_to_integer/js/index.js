57c7f09ee41ada71207e36aebfe81cd334b3737e
Neisser <villa.neisser@gmail.com>
2023-08-13 14:09:41 -0500

    feat: roman to integer code + tests

---
+++ roman_to_integer/js/index.js
@@ -0,0 +1,8 @@
+const { romanToInt } = require('./roman_to_integer');
+
+const example1 = romanToInt('III');
+const example2 = romanToInt('LVIII');
+const example3 = romanToInt('MCMXCIV');
+
+// eslint-disable-next-line no-console
+console.log({ example1, example2, example3 });
